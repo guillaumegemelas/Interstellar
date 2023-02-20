6abfe6a405efc3923d0b5883c511ca91048ffaf8
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-20 13:59:01 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Constants } from "expo-constants";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>Interstellar</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt: {
+    fontSize: 40,
   },
 });
